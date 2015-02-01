2ddefa7cf965871ecc37c4c19d65f7bfefe656fe
Pablo <pabloromanh@gmail.com>
2015-02-01 13:49:58 +0100

    Fixed iqiyi player and added more content.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,48 @@ $(document).ready(function() {
 	var videos = [
 		{
 			source: 'youtube',
-			id: 'nIji3qgEFsc',
+			id: 'r2PoBAZeWPo',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'g1na24Bd5Rs',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'iqiyi',
+			id: 'b6d23fec3c24ae0a61dba33eb34a5202/0/0/w_19rrepgucl.swf-albumId=897022409-tvId=897022409-isPurchase=0-cnId=2',
 			date: '2015-02-01T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'DIPGLKZ76xk',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'NdzGQInkg-w',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'kypaX1ch7g0',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'kBADhKRujdM',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'yelfxueMlvc',
+			date: '2015-02-02T08:06:57.718Z'
+		},
+		{
+			source: 'youtube',
+			id: 'mDHIAY1h-3M',
+			date: '2015-02-02T08:06:57.718Z'
 		},
 		{
 			source: 'youtube',
@@ -13,12 +53,12 @@ $(document).ready(function() {
 		},
 		{
 			source: 'youtube',
-			id: 'I1_hdPqRA_Q',
+			id: 'nIji3qgEFsc',
 			date: '2015-02-01T08:06:57.718Z'
 		},
 		{
-			source: 'iqiyi',
-			id: 'b6d23fec3c24ae0a61dba33eb34a5202/0/0/w_19rrepgucl',
+			source: 'youtube',
+			id: 'I1_hdPqRA_Q',
 			date: '2015-02-01T08:06:57.718Z'
 		}
 	];
